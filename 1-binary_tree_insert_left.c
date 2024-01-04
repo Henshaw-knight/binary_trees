205400e55fb3b888392a0c3a98dd248cba64c671
Henshaw <josephhensh2000@gmail.com>
2024-01-04 12:39:19 -0800

    Update file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,14 +28,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	if (parent->left == NULL)
-		parent->left = new_node;
-	else
+	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
-		parent->left = new_node;
 		new_node->left->parent = new_node;
 	}
+	parent->left = new_node;
 
 	return (new_node);
 }
